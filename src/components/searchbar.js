681c44854ed0948f1d833ff4421c05a89a6ff169
Brian Sweeney <bsweeney930@gmail.com>
2017-04-11 12:55:45 -0400

    refactored functional SearchBar component into a class based component

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';//JSX compiles to React.create element, hence why we require React even though it is not explicitly used.
 
-const Searchbar = () => {
-  return <input/> //JSX compiles to React.create element, hence why we require React even though it is not explicitly used.
+class Searchbar extends Component {
+  render() {
+    return <input />
+  }
 };
 
 export default Searchbar; //any component that imports Searchbar, will get the searchbar component.
