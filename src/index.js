fb5d58260af7c98063a0056f007142fd06e4c738
Brian Sweeney <bsweeney930@gmail.com>
2017-04-12 16:39:47 -0400

    youtube API test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/searchBar';
 
 const config = require('../config');
+const API_KEY = config.API_KEY
 
-
+YTSearch({key: API_KEY, term: 'daft punk'}, function(data){
+  console.log(data)
+})
 //create new component that will produce HTML
 
 const App = () => {
